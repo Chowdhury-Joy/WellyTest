eef2f34f0bf19258261ef486771ad9c810007502
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-04-10 23:47:45 +0600

    index page 01(not complete yet)

---
+++ js/nav-whole.js
@@ -0,0 +1,12 @@
+//!! Search..........
+
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
+
