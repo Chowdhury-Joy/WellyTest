48adb0670a3f7fe0ccd0b088ce370b7a7d5f2c42
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-04-14 20:46:24 +0600

    gsap part 1 done

---
+++ js/index.js
@@ -0,0 +1,65 @@
+var tl1 = gsap.timeline({
+          scrollTrigger: {
+                    trigger:'.tl1',
+                    toggleActions: "play pause resume reset",
+                    start: "top 80%"
+          }
+});
+tl1.from('.tl1-img', { y: 500, opacity: 0, duration: .8})
+.from('.tl1-txt-tit', { y: 500, opacity: 0, duration: .8}, "<.2")
+.from('.tl1-txt-des', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+.from('.tl1-txt-btn', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+
+
+var tl2 = gsap.timeline({
+          scrollTrigger: {
+                    trigger:'.tl2',
+                    toggleActions: "play pause resume reset",
+                    start: "top 80%"
+          }
+});
+tl2.from('.tl2-img', { y: 500, opacity: 0, duration: .8})
+.from('.tl2-txt-tit', { y: 500, opacity: 0, duration: .8}, "<.2")
+.from('.tl2-txt-des', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+.from('.tl2-txt-btn', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+
+
+var tl3 = gsap.timeline({
+          scrollTrigger: {
+                    trigger:'.tl3',
+                    toggleActions: "play pause resume reset",
+                    start: "top 80%"
+          }
+});
+tl3.from('.tl3-img', { y: 500, opacity: 0, duration: .8})
+.from('.tl3-txt-tit', { y: 500, opacity: 0, duration: .8}, "<.2")
+.from('.tl3-txt-des', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+.from('.tl3-txt-btn', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+
+
+var tl4 = gsap.timeline({
+          scrollTrigger: {
+                    trigger:'.tl4',
+                    toggleActions: "play pause resume reset",
+                    start: "top 80%"
+          }
+});
+tl4.from('.tl4-img', { y: 500, opacity: 0, duration: .8})
+.from('.tl4-txt-tit', { y: 500, opacity: 0, duration: .8}, "<.2")
+.from('.tl4-txt-des', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+.from('.tl4-txt-btn', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+
+
+var tl5 = gsap.timeline({
+          scrollTrigger: {
+                    trigger:'.tl5',
+                    toggleActions: "play pause resume reset",
+                    start: "top 80%"
+          }
+});
+tl5.from('.tl5-img', { y: 500, opacity: 0, duration: .8})
+.from('.tl5-txt-tit', { y: 500, opacity: 0, duration: .8}, "<.2")
+.from('.tl5-txt-des', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+.from('.tl5-txt-btn', { y: 500, opacity: 0, duration: .8, ease: "sine.out"}, "<")
+
+
--- js/nav-whole.js
+++ js/nav-whole.js
@@ -1,12 +1,21 @@
+//!! Navbar for mobile
+
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+  navUL.classList.toggle('show');
+});
+
 //!! Search..........
 
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
+// const input = document.getElementById("search-input");
+// const searchBtn = document.getElementById("search-btn");
 
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
+// const expand = () => {
+//   searchBtn.classList.toggle("close");
+//   input.classList.toggle("square");
+// };
 
-searchBtn.addEventListener("click", expand);
+// searchBtn.addEventListener("click", expand);
 
