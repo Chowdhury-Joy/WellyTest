1f42bc3a4d00ddeb7e2cf4f054834a9cb10e768e
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-05-01 09:18:55 +0600

    complete v1

---
+++ js/itt.js
@@ -0,0 +1,74 @@
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.main-itt',
+        pin: '.main-itt',
+        start: 'top 20%',
+        scrub: 1
+    }
+});
+tl.to('.itt-img_01', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_02', 4, {
+        autoAlpha: 0
+    }, '-=4')
+    .from('.itt-img_03', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_04', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_05', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_06', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_07', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_08', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_09', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_10', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_11', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_12', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_13', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_14', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_15', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_16', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_17', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_18', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_19', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_20', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_21', 4, {
+        autoAlpha: 0
+    })
+    .from('.itt-img_22', 4, {
+        autoAlpha: 0
+    })
